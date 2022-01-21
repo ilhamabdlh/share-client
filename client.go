a81880df6514fa31d1a4973f853506af6edd264d
Ilham Abdullah <72017753+ilhamabdlh@users.noreply.github.com>
2022-01-21 13:59:54 +0700

    init

--- client.go
+++ client.go
@@ -1,94 +1,85 @@
 package main
 
 import (
-	// "bytes"
-	"fmt"
-	"io/ioutil"
-	"log"
-
 	"golang.org/x/crypto/ssh"
-	kh "golang.org/x/crypto/ssh/knownhosts"
-
-	"sshserver/commands"
-	"sshserver/utils"
-	"sshserver/colors"
-	"golang.org/x/term"
-	sess "github.com/gliderlabs/ssh"
+	// "golang.org/x/crypto/ssh/knownhosts"
+	"io"
+	"log"
+	"os"
+	"io/ioutil"
+	// "fmt"
 )
 
 func main() {
-	user := "synth"
-	address := "192.168.254.152"
-	command := "uptime"
-	port := "2222"
+
+	// Create client config
+	// var hostt = ""
+	// var users = ""
+
+	// fmt.Print("host: ")
+	// fmt.Scanf("%s\n", &hostt)
+	// fmt.Print("user: ")
+	// fmt.Scanf("%s\n", &users)
 
 	key, err := ioutil.ReadFile("/Users/synth/.ssh/id_rsa")
 	if err != nil {
 		log.Fatalf("unable to read private key: %v", err)
 	}
-
-	// Create the Signer for this private key.
-	signer, err := ssh.ParsePrivateKey(key)
-	if err != nil {
-		log.Fatalf("unable to parse private key: %v", err)
+	signer, errs := ssh.ParsePrivateKey(key)
+	if errs != nil {
+		log.Fatalf("unable to parse private key: %v", errs)
 	}
-
-	hostKeyCallback, err := kh.New("/Users/synth/.ssh/known_hosts")
-	if err != nil {
-		log.Fatal("could not create hostkeycallback function: ", err)
-	}
+	// var hostkeyCallback ssh.HostKeyCallback
+	// hostkeyCallback, err = knownhosts.New("/Users/synth/.ssh/known_hosts")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
 
 	config := &ssh.ClientConfig{
-		User: user,
-		Auth: []ssh.AuthMethod{
-			// Add in password check here for moar security.
-			ssh.PublicKeys(signer),
-		},
-		HostKeyCallback: hostKeyCallback,
+		User: "synth",
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Auth: []ssh.AuthMethod{ssh.PublicKeys(signer)},
 	}
-	// Connect to the remote server and perform the SSH handshake.
-	client, err := ssh.Dial("tcp", address+":"+port, config)
+	// Connect to ssh server
+	conn, err := ssh.Dial("tcp", "192.168.254.152:2222", config)
 	if err != nil {
-		log.Fatalf("unable to connect: %v", err)
+		log.Fatalf("unable to connect: %s", err)
 	}
-	defer client.Close()
-	ss, err := client.NewSession()
+	defer conn.Close()
+	// Create a session
+	session, err := conn.NewSession()
 	if err != nil {
-		log.Fatal("unable to create SSH session: ", err)
+		log.Fatalf("unable to create session: %s", err)
 	}
-	defer ss.Close()
-	// Creating the buffer which will hold the remotly executed command's output.
-	// var stdoutBuf bytes.Buffer
-	// ss.Stdout = &stdoutBuf
-	ss.Run(command)
-	// // Let's print out the result of command.
-	// fmt.Println(stdoutBuf.String())
+	defer session.Close()
+	// Set up terminal modes
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+	// Request pseudo terminal
+	if err := session.RequestPty("vt220", 80, 40, modes); err != nil {
+		log.Fatalf("request for pseudo terminal failed: %s", err)
+	}
 
-	sess.Handle(func(s sess.Session) {
-		utils.Type(s, fmt.Sprintf("%sWelcome to ssh-okra! %s\n\n", colors.Green, colors.Reset))
+	stdin, err := session.StdinPipe()
+	if err != nil {
+		log.Fatalf("Unable to setup stdin for session: %v", err)
+	}
+	go io.Copy(stdin, os.Stdin)
 
-		term := term.NewTerminal(s, fmt.Sprintf("%s[%s@ssh-okra]%s$ ", colors.Green, s.User(), colors.Reset))
+	stdout, err := session.StdoutPipe()
+	if err != nil {
+		log.Fatalf("Unable to setup stdout for session: %v", err)
+	}
+	go io.Copy(os.Stdout, stdout)
 
-		
-				
-		for {
-			commande, err := term.ReadLine()
+	stderr, err := session.StderrPipe()
+	if err != nil {
+		log.Fatalf("Unable to setup stderr for session: %v", err)
+	}
+	go io.Copy(os.Stderr, stderr)
 
-			if err != nil {
-				CloseServerHandler(s)
-				break
-			}
-
-			commands.RunCommand(term, commande, s)
-			log.Println(fmt.Sprintf("%s: \"%s\"", s.RemoteAddr(), commande))
-
-		}
-	})
-}
-
-func CloseServerHandler(s sess.Session) {
-	
-
-	log.Println(fmt.Sprintf("connection from %s closed.", s.RemoteAddr()))
-	// s.Exit(0)
+	err = session.Run("ls -laG /")
 }
